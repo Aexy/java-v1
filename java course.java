8a7c5c68ad2a821814199cacc3b27cf19edf63ac
Aexy <ahomeyro12@gmail.com>
2023-08-30 16:54:44 +0200

    Java introduction

---
+++ java course.java
@@ -0,0 +1,36 @@
+class Main {
+    private static void main (String [] args){
+        //double age = 19.8;
+        //int number3 = (int)age;
+        boolean isStudent = false;
+        boolean isLibraryMember = true;
+        int age = 19;
+        int score = 0;
+        score++;
+        System.out.println(score++);
+        System.out.println(score);
+        // and operator: System.out.println(age >= 18 && age <= 22);
+        // or operator : System.out.println(isStudent || isLibraryMember);
+        // not operator: System.out.println(!isStudent);
+    }
+}
+class Stringmodification{
+    public static void main(String[] args){
+        String name = "Ali";
+        String country = "Turkey";
+        int age = 26;
+        double ibGrade = 29;
+        String formatted = String.format ("My name is %s. I am from %s. I am %d. My IB grade is %f", name, country, age, ibGrade);
+        //System.out.println(formatted);
+        //System.out.println(formatted.length());
+        //System.out.println(formatted.isEmpty());
+        //System.out.println(name.toUpperCase().toLowerCase());
+        //String str1 = new String("abc");
+        //String str2 = new String("Abc");
+        //System.out.println(str1.equalsIgnoreCase (str2));
+        String stringy = "The sky is cloudy.";
+        System.out.println(stringy.replace("cloudy", "red"));
+
+
+    }
+}
