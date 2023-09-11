d1bcbbc11c379d2165ee45add3484a938b832510
Aexy <ahomeyro12@gmail.com>
2023-09-11 15:23:17 +0200

    forloops

--- forloops.java
+++ forloops.java
@@ -1,3 +1,12 @@
-public class forloops {
-    
+class forloops {
+    public static void main(String[] args) {
+        int numbers[] ={1,2,3,4,5,6,7,8,9};
+
+        int sum = 0;
+
+        for (int index = 0; index<numbers.length; index++) {
+        sum += numbers[index];
+    }
+        System.out.println(sum);
+    }
 }
