cd1e89bf7754c890169bc1758959089475a2aa84
Aexy <ahomeyro12@gmail.com>
2023-09-15 16:20:28 +0200

    fix up http server

--- httpserver.java
+++ httpserver.java
@@ -3,16 +3,17 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
 
-public class httpserver {
+public class httpserver {   //http://localhost:8080
     public static void main(String [] args ) throws IOException{
-        ServerSocket server =new ServerSocket(8080);
-        System.out.println("Listening for connection on port 8080... ");
-        while (true){
-            try (Socket socket = server.accept()){
-                Date today = new Date();
-                String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
-                socket.getOutputStream()
-                .write(httpResponse.getBytes("UTF-8"));
+        try (ServerSocket server = new ServerSocket(8080)) {
+            System.out.println("Listening for connection on port 8080... ");
+            while (true){
+                try (Socket socket = server.accept()){
+                    Date today = new Date();
+                    String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+                    socket.getOutputStream()
+                    .write(httpResponse.getBytes("UTF-8"));
+                }
             }
         }
     }
